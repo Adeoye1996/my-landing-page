666733630130f7f683f93e68d700b0dc0119aff2
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-07-05 21:45:33 -0700

    javaScript files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-thedocument.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a');
     
     for (const link of links) {
